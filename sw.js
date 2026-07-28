354ed9af931fde9405af1df091389807e8f08232
ChickenMayCry <chickenmaycry.cmc@gmail.com>
2026-07-28 15:53:53 +0900

    database.jsをキャッシュ対象へ追加

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'purakoro-pwa-v1';
+const CACHE_NAME = 'purakoro-pwa-v2';
 const ASSETS = [
   './',
   './index.html',
+  './database.js',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
